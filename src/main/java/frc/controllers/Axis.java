029778fec0d40d20f38ed3229a5e614ce0e31436
ConnoisseurOfCode <chadchapman3@gmail.com>
2022-02-02 18:36:52 -0500

    Rework deadband feature and add implementation for SmartDashboard.

--- src/main/java/frc/controllers/Axis.java
+++ src/main/java/frc/controllers/Axis.java
@@ -1,20 +1,35 @@
 package frc.controllers;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import edu.wpi.first.wpilibj.Joystick;
 
-
+/**
+ * Class for interfacing with the 2 axes on an XBOX controller.
+ */
 public class Axis 
 {
 	/**
 	 * The point on the axis relative to the origin in which input 
 	 * should start to be registered.
 	 */
-	private final double DEADBAND = 0.1;
+	private static double DEADBAND = 0.1;
+
+	/**
+	 * The value that the final output value will be multiplied by.
+	 */
+	private static double SENSITIVITY = 1;
 
 	private Joystick joystick;
 	private int axis;
 	private boolean invert;
 	
+	static
+	{
+		SmartDashboard.putNumber("CONTROLLER_AXIS_DEADBAND", DEADBAND);
+		SmartDashboard.putNumber("CONTROLLER_AXIS_SENSITIVITY", SENSITIVITY);
+	}
+
 	/**
 	 * Create a new axis on the specified Joystick.
 	 * @param joystick The Joystick to derive the axis from.
@@ -33,17 +48,23 @@ public class Axis
 	 */
 	public double get()
 	{
+		DEADBAND = SmartDashboard.getNumber("CONTROLLER_AXIS_DEADBAND", DEADBAND);
+		SENSITIVITY = SmartDashboard.getNumber("CONTROLLER_AXIS_SENSITIVITY", SENSITIVITY);
+
+		if (DEADBAND == 1 || SENSITIVITY == 0)
+			return 0;
+
 		double value = joystick.getRawAxis(axis);
 
 		if (Math.abs(value) < DEADBAND) 
 			return 0;
 		
-		//Apply deadband constraint: begin input after the deadband.
-		value = (value - DEADBAND) / DEADBAND;
+		//Apply deadband constraint and assess the -1 to 1 output limit.
+		double outputValue = Math.min(Math.max(-1, (value - DEADBAND) / (1 - DEADBAND) * SENSITIVITY), 1);
 		
 		if (this.invert)
-			return -value;
+			return -outputValue;
 		
-		return value;
+		return outputValue;
 	}
 }

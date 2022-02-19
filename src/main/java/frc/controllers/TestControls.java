10460b7ed14ab3ce308a54c0bf38c90e55e09d21
ConnoisseurOfCode <chadchapman3@gmail.com>
2022-02-19 10:23:22 -0500

    Update testing controls for cleanup and use in autonomous debugging.

--- src/main/java/frc/controllers/TestControls.java
+++ src/main/java/frc/controllers/TestControls.java
@@ -2,39 +2,64 @@ package frc.controllers;
 
 import frc.subsystem.Drives;
 
+import frc.drives.DrivesSensorInterface;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 /**
  * Controller for testing.
  */
 public class TestControls extends Controller 
 {
-    private long startTimeStamp;
-    private final long RUNTIME;
+    DrivesSensorInterface drivesSensors;
 
     public TestControls(Drives drives) 
     {
         super(drives);
-        RUNTIME = 10000; //In milliseconds.
-        reset();
-    }
 
-    public void reset()
-    {
-        startTimeStamp = 0;
+        drivesSensors = drives.getSensors();
+
+        SmartDashboard.putNumber("DRIVE_FORWARD", 0);
+        SmartDashboard.putNumber("DRIVE_BACKWARD", 0);
+        SmartDashboard.putNumber("TURN_RIGHT", 0);
+        SmartDashboard.putNumber("TURN_LEFT", 0);
     }
 
     @Override
     public void execute() 
     {
-        if (startTimeStamp == 0)
+        SmartDashboard.putNumber("GYRO_ANGLE", drivesSensors.getGyroAngle());
+
+        SmartDashboard.putNumber("DRIVES_LEFT_ENCODER_DISTANCE", drivesSensors.getLeftEncoderDistance());
+        SmartDashboard.putNumber("DRIVES_LEFT_ENCODER_SPEED", drivesSensors.getLeftEncoderSpeed());
+
+        SmartDashboard.putNumber("DRIVES_RIGHT_ENCODER_DISTANCE", drivesSensors.getRightEncoderDistance());
+        SmartDashboard.putNumber("DRIVES_RIGHT_ENCODER_SPEED", drivesSensors.getRightEncoderSpeed());
+
+        double moveForward = SmartDashboard.getNumber("DRIVE_FORWARD", 0);
+        double moveBackward = SmartDashboard.getNumber("DRIVE_BACKWARD", 0);
+        double turnLeft = SmartDashboard.getNumber("TURN_LEFT", 0);
+        double turnRight = SmartDashboard.getNumber("TURN_RIGHT", 0);
+
+        if (moveForward > 0)
         {
-            startTimeStamp = System.currentTimeMillis();
-            drives.moveBackward(9999);
+            drives.moveForward(moveForward);
+            SmartDashboard.putNumber("DRIVE_FORWARD", 0);
         }
-        else if (startTimeStamp != Long.MAX_VALUE && System.currentTimeMillis() > startTimeStamp + RUNTIME)
+        else if (moveBackward > 0)
         {
-            startTimeStamp = Long.MAX_VALUE;
-            drives.setJoysticks(0, 0);
-            drives.startDriverControlled();
+            drives.moveBackward(moveBackward);
+            SmartDashboard.putNumber("DRIVE_BACKWARD", 0);
+        }
+        else if (turnLeft > 0)
+        {
+            drives.turnLeft(turnLeft);
+            SmartDashboard.putNumber("TURN_LEFT", 0);
+        }
+        else if (turnRight > 0)
+        {
+            drives.turnRight(turnRight);
+            SmartDashboard.putNumber("TURN_RIGHT", 0);
         }
     }
 }

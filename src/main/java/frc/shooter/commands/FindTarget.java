c360c99e0e9c6293a24e2ea30ef89d6c547473c6
ConnoisseurOfCode <chadchapman3@gmail.com>
2022-03-05 11:48:58 -0500

    Added FindTarget command.

---
+++ src/main/java/frc/shooter/commands/FindTarget.java
@@ -0,0 +1,20 @@
+package frc.shooter.commands;
+
+import frc.shooter.ShooterCommand;
+import frc.shooter.ShooterOutput;
+import frc.shooter.ShooterSensorInterface;
+
+public class FindTarget extends ShooterCommand
+{
+    public FindTarget(ShooterSensorInterface sensors)
+    {
+        super(sensors);
+    }
+
+    @Override
+    public ShooterOutput execute() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}

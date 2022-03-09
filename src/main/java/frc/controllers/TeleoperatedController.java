12070c10bef3b1411c1a5f7b464c5af0c0b3b840
ConnoisseurOfCode <chadchapman3@gmail.com>
2022-03-09 15:54:13 -0500

    Add low speed mode if the right trigger is compressed.

--- src/main/java/frc/controllers/TeleoperatedController.java
+++ src/main/java/frc/controllers/TeleoperatedController.java
@@ -13,7 +13,8 @@ public class TeleoperatedController extends Controller
 	private Axis driverLeftAxisY;
   private Axis driverLeftAxisX;
 	private Axis driverRightAxis;
-  
+  private Axis driverRightTrigger;
+
   static 
   {
     SmartDashboard.putBoolean("USE_BOTH_JOYSTICKS", true);
@@ -33,7 +34,8 @@ public class TeleoperatedController extends Controller
     driverLeftAxisY = new Axis(driverJoystick, ControllerMappings.XBOX_LEFT_Y, true);
     driverLeftAxisX = new Axis(driverJoystick, ControllerMappings.XBOX_LEFT_X, true);
     driverRightAxis = new Axis(driverJoystick, ControllerMappings.XBOX_RIGHT_Y, true);
-    
+    driverRightTrigger = new Axis(driverJoystick, ControllerMappings.XBOX_R2, true);
+   
     //Add controls for additional subsystems here.
   }
 
@@ -52,6 +54,19 @@ public class TeleoperatedController extends Controller
         leftAxisX = -leftAxisX;
 
       getDrives().setJoysticks(leftAxisY - leftAxisX, leftAxisY + leftAxisX);
+    }
+
+    if (driverRightTrigger.get() <= -0.8)
+    {
+      driverLeftAxisX.setSensitivity(0.4);
+      driverLeftAxisY.setSensitivity(0.4);
+      driverRightAxis.setSensitivity(0.4);
+    }
+    else
+    {
+      driverLeftAxisX.setSensitivity(1);
+      driverLeftAxisY.setSensitivity(1);
+      driverRightAxis.setSensitivity(1);
     }
   }
 }

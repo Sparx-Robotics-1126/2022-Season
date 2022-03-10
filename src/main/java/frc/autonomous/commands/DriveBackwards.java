42d8f55d9343b61e368d75caf28bfa441cd4cb6e
ConnoisseurOfCode <chadchapman3@gmail.com>
2022-03-10 01:07:49 -0500

    Add autonomous implementation for the Drives DriveBackwards command.

---
+++ src/main/java/frc/autonomous/commands/DriveBackwards.java
@@ -0,0 +1,27 @@
+package frc.autonomous.commands;
+
+import frc.robot.Robot;
+
+import frc.autonomous.AutonomousCommand;
+
+public class DriveBackwards extends AutonomousCommand
+{
+    private double distance;
+
+    public DriveBackwards(double distance)
+    {
+        this.distance = distance;
+    }
+
+    @Override
+    public void execute() 
+    {
+        Robot.getDrives().moveBackward(distance);
+    }
+
+    @Override
+    public boolean isDone() 
+    {
+        return Robot.getDrives().isDone();
+    }
+}
